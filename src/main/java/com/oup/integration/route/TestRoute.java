2c3e9d4f240492cc965bec92267c2f2cfea3b0c0
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2023-07-13 12:41:41 +0100

    NA

---
+++ src/main/java/com/oup/integration/route/TestRoute.java
@@ -0,0 +1,33 @@
+package com.oup.integration.route;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class TestRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+        onException(Exception.class).useOriginalMessage()
+
+                .log(LoggingLevel.ERROR, log, "Excepion occurred in Route. Exception : ${exception.message}")
+
+                .log(LoggingLevel.WARN, log, "${exception.stacktrace}")
+
+                .end();
+        // TODO Auto-generated method stub
+        from("timer://foo?fixedRate=true&period=30000").routeId("id_SampleRoute")
+                .log(LoggingLevel.INFO, log, "Timer Route Started")
+                //.delay(120000)
+                //.wireTap("direct:dummy")
+                .to("direct:dummy");
+
+        from("direct:dummy")
+                .to("https4://reqres.in/api/users/2")
+                .log(LoggingLevel.INFO, log, "${body}");
+
+    }
+}
